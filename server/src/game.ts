012dc8f22615740047379f591a958d32eae1f5f8
Pei Shi Yong <peishiyong@Peis-MacBook-Air-2024.local>
2024-02-04 17:37:03 +1100

    randomly rotate topbottom during shuffle

--- server/src/game.ts
+++ server/src/game.ts
@@ -11,16 +11,28 @@ type Game = {
 }
 
 // copied from internet. Alter the array
-const shuffle = (array: Card[]) => { 
-  for (let i = array.length - 1; i > 0; i--) { 
-    const j = Math.floor(Math.random() * (i + 1)); 
-    [array[i], array[j]] = [array[j], array[i]]; 
-  } 
-  return array; 
-}; 
+const shuffle = (array: Card[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const rotateTopBottom = (array: Card[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomBool = Math.random() > 0.5 ? true : false;
+    if (randomBool) {
+      const originalTop = array[i].top;
+      const originalBottom = array[i].bottom;
+      array[i] = { top: originalBottom, bottom: originalTop }
+    }
+  }
+  return array;
+}
 
 export function newGame(): Game {
-  const shuffledDeck = shuffle(deck);
+  const shuffledDeck = rotateTopBottom(shuffle(deck));
 
   let hand1 = shuffledDeck.slice(0, (0+11));
   let hand2 = shuffledDeck.slice(11, (11+11));
