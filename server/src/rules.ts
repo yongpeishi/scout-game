257e58a10e3d27b9c4d3c2564173cf1f33bd3fab
Pei Shi Yong <peishiyong@Peis-MacBook-Air-2024.local>
2024-02-28 21:42:00 +1100

    valid show can be set or sequence

--- server/src/rules.ts
+++ server/src/rules.ts
@@ -1,5 +1,30 @@
+import { produce } from "immer";
 import { Card } from "./types"
 
+const isSet = (cards: Card[]): boolean => {
+  const firstTopNumber = cards[0].top;
+  const isSameTopNumber = (card: Card) => card.top == firstTopNumber;
+
+  return cards.every(isSameTopNumber);
+}
+
+const isAscending = (cards: Card[]): boolean => {
+  const isCurrentSmallerThanNext = (card: Card, index: number) => {
+    return index === cards.length - 1 || card.top < cards[index + 1].top;
+  }
+
+  return cards.every(isCurrentSmallerThanNext);
+}
+
+const isDescending = (cards: Card[]): boolean => {
+  const reversedCards = produce(cards, draft => draft.reverse())
+  return isAscending(reversedCards);
+}
+
 export const isValidShow = (cards: Card[]):boolean => {
-  return cards.length == 1 ? true : false
+  if (cards.length == 1 || isSet(cards) || isAscending(cards) || isDescending(cards)) {
+    return true;
+  } else {
+    return false
+  }
 }
--- server/tests/unit-tests/rules.spec.ts
+++ server/tests/unit-tests/rules.spec.ts
@@ -9,15 +9,31 @@ describe('rules', () => {
       expect(isValidShow(cards)).toBe(true)
     })
 
-    it('true if set of 1 or more cards', () => {
+    it('true if set', () => {
+      const cards = [
+        { top: 5, bottom: 3 },
+        { top: 5, bottom: 2 },
+        { top: 5, bottom: 7 }
+      ];
+      expect(isValidShow(cards)).toBe(true)
     })
 
     it('true if ascending sequence', () => {
-
+      const cards = [
+        { top: 4, bottom: 3 },
+        { top: 5, bottom: 2 },
+        { top: 6, bottom: 7 }
+      ];
+      expect(isValidShow(cards)).toBe(true)
     })
 
     it('true if descending sequence', () => {
-
+      const cards = [
+        { top: 8, bottom: 3 },
+        { top: 7, bottom: 2 },
+        { top: 6, bottom: 7 }
+      ];
+      expect(isValidShow(cards)).toBe(true)
     })
 
     it('false if not a set nor sequence', () => {
@@ -26,7 +42,7 @@ describe('rules', () => {
         { top: 1, bottom: 2 },
         { top: 4, bottom: 7 },
         { top: 8, bottom: 6 },
-        { top: 9, bottom: 3 },
+        { top: 9, bottom: 3 }
       ];
       expect(isValidShow(cards)).toBe(false)
     })
